413b379dee72489a9db1c9580b38768f55878382
Leo Du <dul2@uw.edu>
2018-05-30 01:51:47 -0700

    Tune

--- GAN/MNIST/GAN.py
+++ GAN/MNIST/GAN.py
@@ -1,13 +1,9 @@
 from GAN.utils import *
 
-NOISE_DIM = 96
-IMG_DIM = 28
-IMG_CHAN = 1
-
 def get_discriminator():
     return nn.Sequential(
         Flatten(),
-        nn.Linear(IMG_DIM * IMG_DIM * IMG_CHAN, 256),
+        nn.Linear(1*28*28, 256),
         nn.LeakyReLU(0.01, inplace=True),
         nn.Linear(256, 256),
         nn.LeakyReLU(0.01, inplace=True),
@@ -15,13 +11,13 @@ def get_discriminator():
     )
 
 
-def get_generator(noise_dim=NOISE_DIM):
+def get_generator(noise_dim=96):
     return nn.Sequential(
         nn.Linear(noise_dim, 1024),
         nn.ReLU(inplace=True),
         nn.Linear(1024, 1024),
         nn.ReLU(inplace=True),
-        nn.Linear(1024, IMG_DIM * IMG_DIM * IMG_CHAN),
+        nn.Linear(1024, 1*28*28),
         nn.Tanh()
     )
 
