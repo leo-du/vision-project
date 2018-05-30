13f909c96b435d8b17e5731a774a057e0d00e06c
Leo Du <dul2@uw.edu>
2018-05-30 01:27:41 -0700

    Add optimizer

--- GAN/utils.py
+++ GAN/utils.py
@@ -1,35 +1,12 @@
 import torch
 import torch.nn as nn
+import torch.optim as optim
 from torch.nn import init
-
-import numpy as np
-
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
 
 
 def initialize_weights(m):
     if isinstance(m, nn.Linear) or isinstance(m, nn.ConvTranspose2d):
         init.xavier_uniform_(m.weight.data)
-
-
-def show_images(images):
-    images = np.reshape(images, [images.shape[0], -1])
-    sqrtn = int(np.ceil(np.sqrt(images.shape[0])))
-    sqrtimg = int(np.ceil(np.sqrt(images.shape[1])))
-
-    fig = plt.figure(figsize=(sqrtn, sqrtn))
-    gs = gridspec.GridSpec(sqrtn, sqrtn)
-    gs.update(wspace=0.05, hspace=0.05)
-
-    for i, img in enumerate(images):
-        ax = plt.subplot(gs[i])
-        plt.axes('off')
-        ax.set_xticklabels([])
-        ax.set_yticklabels([])
-        ax.set_aspect('equal')
-        plt.imshow(img.reshape([sqrtimg, sqrtimg]))
-    return
 
 
 def bce_loss(logits, target):
@@ -50,6 +27,8 @@ def generator_loss(logits_fake):
 def sample_noise(batch_size, dim):
     return torch.rand(batch_size, dim) * 2.0 - 1.0
 
+def get_optimizer(model):
+    return optim.Adam(model.parameters(), 1e-3, (0.5, 0.999))
 
 class Flatten(nn.Module):
     def forward(self, x):
